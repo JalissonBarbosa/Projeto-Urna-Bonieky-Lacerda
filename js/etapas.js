75500b84adc745845454607944b4d7b944485fef
JalissonBarbosa <jalissonbcampos@gmail.com>
2020-12-08 12:51:47 -0300

    commit

---
+++ js/etapas.js
@@ -0,0 +1,52 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [{
+            numero: '00002',
+            nome: 'Cascão',
+            partido: 'Anti-Monica',
+            fotos: [{ url: 'cascao.png', legenda: 'Vereador', small: false }]
+        },
+
+        {
+            numero: '00001',
+            nome: 'Magali',
+            partido: 'Anti-Cascão',
+            fotos: [{ url: 'magali.png', legenda: 'Vereador' }]
+        },
+        ]
+    },
+
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '11',
+                nome: 'Monica',
+                partido: 'Anti-Cascão',
+                vice: 'Magali',
+                fotos: [{ url: 'monica.png', legenda: 'Prefeito' }, { url: 'magali.png', legenda: 'Vice-Prefeito', small: true }]
+            },
+
+            {
+                numero: '22',
+                nome: 'Chico Bento',
+                partido: 'Anti-Monica',
+                vice: 'Cascão',
+                fotos: [{ url: 'cascao.png', legenda: 'Prefeito' }, { url: 'chicobento.png', legenda: 'Vice-Prefeito', small: true }]
+            },
+        ]
+    }
+
+
+
+
+
+
+
+
+
+
+];
